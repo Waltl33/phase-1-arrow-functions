61aa6b99eeb0d4266d5d575c3f994ea4af1f03e5
Walter <walter.clark24@yahoo.com>
2022-12-12 17:07:50 -0500

    commit tree 806ef3b163dd75ce6a0ccc893ea0cc5d035947e5

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = () => {
+let division = 2000 / 100
+return division
+}
+
+const square = (num1) =>{
+let multiply = 2
+return multiply ** multiply
+}
+
+const add = (num1, num2)=>{
+
+    result = num1 + num2
+    return result
+}
